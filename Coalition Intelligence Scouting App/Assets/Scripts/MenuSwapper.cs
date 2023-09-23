780f3199be34a83a7c1008e9f5fd86d2f716e088
Sammy-Da-Great <sammyhusma@gmail.com>
2023-09-22 17:32:15 -0700

    Edited MenuSwapper

--- Coalition Intelligence Scouting App/Assets/Scripts/MenuSwapper.cs
+++ Coalition Intelligence Scouting App/Assets/Scripts/MenuSwapper.cs
@@ -5,22 +5,22 @@ using UnityEngine.Events;
 
 public class MenuSwapper : MonoBehaviour
 {
-    // Allows you to use this script with a dropdown
     [SerializeField] public TMP_Dropdown menuSelectionDropdown;
-    // List of all menus, edited in inspector
-    [SerializeField] public List<Menu> menus;
+    [SerializeField] public List<Menu> menus = new();
 
     private void Awake()
     {
-        // Used to automatically change menus when the dropdown changes to the menu selected in the dropdown
-        if (menuSelectionDropdown != null) {
+        if (menuSelectionDropdown != null)
+        {
+            // Used to automatically change menus when the dropdown changes to the menu selected in the dropdown
             menuSelectionDropdown.onValueChanged.AddListener(delegate { ChangeMenu(menuSelectionDropdown.value); });
+
             UpdateDropdown();
         }
     }
 
     // Updates options of dropdown based on menu list
-    //[EditorCools.Button]
+    [EditorCools.Button]
     public void UpdateDropdown()
     {
         // Prevents running if there is no dropdown so it shows a warning instead of an error
@@ -45,13 +45,36 @@ public class MenuSwapper : MonoBehaviour
         // Allows invoking the events that you you wanted invoked upon changing to that menu
         menus[menuIndex].onMenuSelect.Invoke();
     }
+
+    [EditorCools.Button]
+    public void AddChildren()
+    {
+        int initialSize = menus.Count;
+
+        for (int a = 0; a < transform.childCount; a++)
+        {
+            menus.Add(new Menu());
+            // Sets it's name to the gameObject to more easily discern which one it is
+            menus[a + initialSize].name = transform.GetChild(a).name;
+
+            for (int b = 0; b < transform.childCount; b++)
+            {
+                GameObject child = transform.GetChild(b).gameObject;
+                // Adds the MenuParts to disable the other child menus and enable this one
+                menus[a + initialSize].menuParts.Add(new MenuPart()
+                { gameObject = child, isEnabled = child.name == menus[a + initialSize].name });
+            }
+        }
+
+        UpdateDropdown();
+    }
 }
 
 [System.Serializable]
 public class Menu
 {
     public string name;
-    public List<MenuPart> menuParts;
+    public List<MenuPart> menuParts = new();
     public UnityEvent onMenuSelect;
 }
 
