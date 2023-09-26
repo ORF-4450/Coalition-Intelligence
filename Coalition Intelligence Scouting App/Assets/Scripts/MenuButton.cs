471d4f571d2d20c9cb248cad46e79e8371c7bd9d
Sammy-Da-Great <sammyhusma@gmail.com>
2023-09-25 17:52:25 -0700

    Started Menu Button Color Scripts

---
+++ Coalition Intelligence Scouting App/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] int MenuID;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
